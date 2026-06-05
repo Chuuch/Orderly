e16dfcb819045c17c332b3fda45b7e0080427d38
Chuuch <daniel.chuchulev96@gmail.com>
2026-06-05 11:58:06 +0300

    fix: correct UserRepository to use UUID as ID type and fix typo in method name

--- src/main/java/com/chuch/Orderly/domain/user/repository/UserRepository.java
+++ src/main/java/com/chuch/Orderly/domain/user/repository/UserRepository.java
@@ -9,9 +9,9 @@ import org.springframework.stereotype.Repository;
 import com.chuch.Orderly.domain.user.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
-    boolean eistsByEmailAndRestaurantId(String email, UUID restaurantId);
+    boolean existsByEmailAndRestaurantId(String email, UUID restaurantId);
     Optional<User> findByEmailAndRestaurantId(String email, UUID restaurantId);
 }
