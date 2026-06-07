70b4c8a413bb942deeb0499798c66ffe763bf543
Chuuch <daniel.chuchulev96@gmail.com>
2026-06-07 15:45:32 +0300

    config

--- src/main/java/com/chuch/Orderly/global/config/SecurityConfig.java
+++ src/main/java/com/chuch/Orderly/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.chuch.Orderly.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -34,6 +35,7 @@ public class SecurityConfig {
     public SecurityFilterChain prodSecurityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/v1/auth/**").permitAll()
